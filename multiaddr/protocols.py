740d495d854ad1e95f5bb3ce1ebf0c53aa6e825f
lla-dane <abhinavagarwalla6@gmail.com>
2025-10-07 14:59:26 +0530

    Add noise configs in the registry

--- multiaddr/protocols.py
+++ multiaddr/protocols.py
@@ -153,6 +153,7 @@ PROTOCOLS = [
     Protocol(P_DNS6, "dns6", "domain"),
     Protocol(P_DNSADDR, "dnsaddr", "domain"),
     Protocol(P_SNI, "sni", "domain"),
+    Protocol(P_NOISE, "noise", None),
     Protocol(P_SCTP, "sctp", "uint16be"),
     Protocol(P_UDT, "udt", None),
     Protocol(P_UTP, "utp", None),
